7ed69cab262a1a76642097f52002a4d03ec84ff1
Sean McGowan <(none)>
2016-10-05 19:35:22 -0400

    Add initial integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,26 @@ describe 'ga_proxy_google::default' do
     skip 'Replace this with meaningful tests'
   end
 end
+
+describe command('cat /etc/centos-release') do
+  its(:stdout) { should match(/\s6\.7/) }
+end
+
+describe package('haproxy') do
+  it { should be_installed }
+end
+
+describe service('haproxy') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command('haproxy -v') do
+  its(:stdout) { should match(/\s1\.[56]\./) }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe '
