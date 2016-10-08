c089a478cd48ff7b01e95606f8ee4162ddfcfa3b
Sean McGowan <spmcgowan@gmail.com>
2016-10-08 10:41:38 -0400

    Update metadata with email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'ga_proxy_google'
 maintainer 'Sean McGowan'
-maintainer_email 'you@example.com'
+maintainer_email 'spmcgowan@gmail.com'
 license 'all_rights'
 description 'Installs/Configures ga_proxy_google'
 long_description 'Installs/Configures ga_proxy_google'
