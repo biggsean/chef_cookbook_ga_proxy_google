86e421c00f0949974f00d0b1178a11a454651b0c
Sean McGowan <(none)>
2016-10-07 23:53:44 -0400

    Add ga_haproxy resource to recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,31 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+frontends = {
+  main: {
+    ip: '*',
+    port: 80,
+    default_backend: 'google'
+  }
+}
+backends = {
+  google: {
+    servers: [
+      google1: {
+        socket: 'www.google.com:80',
+        options: ['check']
+      }
+    ],
+    options: [
+      'option  httpchk  HEAD / HTTP/1.1\r\nHost:\ www.google.com',
+      'http-request set-header Host www.google.com',
+      'http-request set-header User-Agent GoogleProxy'
+    ]
+  }
+}
+
+ga_haproxy 'default' do
+  frontends frontends
+  backends backends
+  action :create
+end
