fc4e0a42df27550f28f7da5989acfa6483bd9902
Sean McGowan <(none)>
2016-10-07 23:52:40 -0400

    Add chefspec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -9,12 +9,16 @@ require 'spec_helper'
 describe 'ga_proxy_google::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'creates haproxy service' do
+      expect(chef_run).to create_ga_haproxy('default')
     end
   end
 end
