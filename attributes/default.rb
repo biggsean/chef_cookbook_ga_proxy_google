435ae9be1e6b1a8215eb1701f8e8ff15bbdca503
Sean McGowan <spmcgowan@gmail.com>
2016-10-14 11:09:38 -0400

    Update recipe and attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,22 +1,15 @@
-default['haproxy']['frontends'] = {
-  main: {
-    ip: '*',
-    port: 80,
-    default_backend: 'google'
+default['haproxy']['frontends']['main']['socket'] = '*:80'
+default['haproxy']['frontends']['main']['default_backend'] = 'google'
+
+default['haproxy']['backends']['google']['servers'] = [
+  {
+    name: 'google1',
+    socket: 'www.google.com:80',
+    options: ['check']
   }
-}
-default['haproxy']['backends'] = {
-  google: {
-    servers: [
-      google1: {
-        socket: 'www.google.com:80',
-        options: ['check']
-      }
-    ],
-    options: [
-      'option  httpchk  HEAD / HTTP/1.1\r\nHost:\ www.google.com',
-      'http-request set-header Host www.google.com',
-      'http-request set-header User-Agent GoogleProxy'
-    ]
-  }
-}
+]
+default['haproxy']['backends']['google']['options'] = [
+  'option  httpchk  HEAD / HTTP/1.1\r\nHost:\ www.google.com',
+  'http-request set-header Host www.google.com',
+  'http-request set-header User-Agent GoogleProxy'
+]
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,22 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 Sean McGowan, All Rights Reserved.
-frontends = node['haproxy']['frontends']
-backends = node['haproxy']['backends']
-ga_haproxy 'default' do
-  frontends frontends
-  backends backends
-  action :create
+ga_haproxy 'default'
+
+frontend_name = 'main'
+frontend = node['haproxy']['frontends'][frontend_name]
+ga_haproxy_frontend frontend_name do
+  instance_name 'default'
+  socket frontend['socket']
+  default_backend frontend['default_backend']
+  action :enable
+end
+
+backend_name = 'google'
+backend = node['haproxy']['backends'][backend_name]
+ga_haproxy_backend backend_name do
+  instance_name 'default'
+  servers backend['servers']
+  options backend['options']
+  action :enable
 end
